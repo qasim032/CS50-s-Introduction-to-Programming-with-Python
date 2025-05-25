ab2a86b45d9071ee30c2ccca7d78a488640e95af
codr <syedmmqasim03213@gmail.com>
2025-05-24 22:30:16 -0700

    Sep

--- Python projects/Basic/BMICalculator.py
+++
@@ -1,4 +0,0 @@
-weight = int(input("What's your weight in pounds ? "))
-height = int(input("What's your height in inches ? "))
-BMI = (weight*703)/(height*height)
-print(f"Your BMI is {BMI}")
--- Python projects/Basic/Multiplication.py
+++
@@ -1,4 +0,0 @@
-number = int(input("What is the number? "))
-print(f"The table of {number} is: ")
-for i in range(1,11):
-    print(f"{number} * {i} = {number*i}")
