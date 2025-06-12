a1577ff28c0bd06bba303e239846f061b2cd7755
codr <syedmmqasim03213@gmail.com>
2025-06-12 00:35:16 -0700

    listtuple

---
+++ PY Programs/WaysTuple.py
@@ -0,0 +1,7 @@
+a = 1,2,3,4
+print(type(a))
+#<class 'tuple'>
+print(a[0:4:2])
+# Stating(index 0)
+# Ending index 3
+# increament 2 
--- PY Programs/list.py
+++ PY Programs/list.py
@@ -7,10 +7,14 @@
 #     25:"Larkana"
 # }
 # print(students[25])
-students = [
-    {"Name":"Qasim","Fathername":"Asif","Department":"AI"},
-    {"Name":"Ishaque","Fathername":"Abdul Rehman","Department":"AI"},
-]
-for i in students:
-    print(i["Name"],i["Fathername"])
-                
+# students = [
+#     {"Name":"Qasim","Fathername":"Asif","Department":"AI"},
+#     {"Name":"Ishaque","Fathername":"Abdul Rehman","Department":"AI"},
+# ]
+# for i in students:
+#     print(i["Name"],i["Fathername"])
+name = ["Suleman","Ishaque","Safeer","Shaheer"]
+print(f"Hello { name[0]} How are you?")
+print(f"Hello {name[1]} How are you?")
+print(f"Hello {name[2]} How are you?")
+print(f"Hello {name[3]} How are you?")
