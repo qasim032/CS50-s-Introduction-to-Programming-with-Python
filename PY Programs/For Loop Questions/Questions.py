0c9ac37d96f9d7acc43620af0fde59da7c14b455
codr <syedmmqasim03213@gmail.com>
2025-07-06 09:00:17 -0700

    Setting up files in a folder

---
+++ PY Programs/For Loop Questions/Questions.py
@@ -0,0 +1,47 @@
+# n = int(input("Please enter a number "))
+# for i in range(n):
+#     print("Hello  world")
+
+# n = int(input("Please enter a number "))
+# for i in range(1,n+1):
+#     print(i)
+
+# n = int(input("Please enter a number "))
+# for i in range(n,0,-1):
+#     print(i)
+
+# n = int(input("Please enter a number "))
+# for i in range(1,11):
+#     print(f"{n} * {i} = {n*i}")
+
+# n = int(input("Please enter a number "))
+# sum = 0
+# for i in range(1,n+1):
+#     sum = sum + i
+# print(f"The sum till n num is {sum}")
+
+# odd = 0
+# even = 0 
+# n = int(input("Please enter a number "))
+# for i in range(1,n+1):
+#     if i%2==0:
+#         even = even + i
+#     else:
+#         odd = odd + i
+# print(f"The sum of odd number is {odd}")
+# print(f"The sum of even number is {even}")
+
+
+# name = input("Enter name:- ")
+# rev = name[::-1]
+# if name==rev:
+#     print("It is a palindrome string")
+# else:
+#     print("It's not a palindrome string")
+
+# name = input("Enter name:- ")
+# for i in range(len(name)-1,-1,-1):
+#     print(name[i],end="")
+
+
+
--- PY Programs/Programs practice/saquareroot.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number: "))
-print("The square root of", n, "is", n ** 0.5)
-
-
-# using math module
-import math
-n = int(input("Enter a number: "))
-sq = math.sqrt(n)
-print("The square root of", n, "is", sq)
